98380ce22253828afb13fae423f0071ead51ef51
Govardhini L <124780139+Govardhini06042005@users.noreply.github.com>
2025-12-26 10:26:29 +0530

    Time: 248 ms (93.28%), Space: 66.7 MB (30.56%) - LeetHub

---
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    pattern=r'(^|\s)DIAB1'
+    return patients[patients['conditions'].str.contains(pattern,regex=True)]
+    
